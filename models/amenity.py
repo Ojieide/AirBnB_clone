0fd6b232ae1dfbd42dfc738206568c79d662763d
Ojay <47607681+Ojieide@users.noreply.github.com>
2023-02-11 12:27:57 +0100

    Delete models directory

--- models/amenity.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-""" The amenities of place """
-from . base_model import BaseModel
-
-
-class Amenity(BaseModel):
-    """ Amenity (Comodidades) """
-    name = ''
--- models/state.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-""" State where user's come from """
-from . base_model import BaseModel
-
-
-class State(BaseModel):
-    """ Define the state of the user """
-    name = ''
